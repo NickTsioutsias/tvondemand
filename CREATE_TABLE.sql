eb763db3fba9aa8118400d30f9f0a4346e6cd8b2
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2022-11-14 12:40:11 +0200

    syntax err on user, changed user to xrhsths

--- CREATE_TABLE.sql
+++ CREATE_TABLE.sql
@@ -130,8 +130,8 @@ CREATE TABLE address (
 
 
 CREATE TABLE customer (
-  `customer_id` smallint(5) UNSIGNED NOT NULL,
-  `address_id` smallint(5) UNSIGNED NOT NULL,
+  customer_id smallint(5) UNSIGNED NOT NULL,
+  address_id smallint(5) UNSIGNED NOT NULL,
   PRIMARY KEY (customer_id),
   CONSTRAINT usrcsu FOREIGN KEY (customer_id) REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
   CONSTRAINT CONSTRAINT fk_customer_address FOREIGN KEY (address_id) REFERENCES address (address_id) ON DELETE RESTRICT ON UPDATE CASCADE 
@@ -151,17 +151,16 @@ CREATE TABLE administrator (
   CONSTRAINT usradm FOREIGN KEY (admin_id) REFERENCES user (user_id) ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-create table user(
+CREATE TABLE xrhsths(
   user_id smallint(5) UNSIGNED NOT NULL,
   first_name varchar(45) NOT NULL,
   last_name varchar(45) NOT NULL,
   email varchar(50) DEFAULT NULL,
   active tinyint(1) NOT NULL DEFAULT 1,
-  create_date datetime NOT NULL
+  create_date datetime NOT NULL,
   PRIMARY KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
---
+
 
 CREATE TABLE customer_subscription(
 	customer_id SMALLINT UNSIGNED NOT NULL,
