fec46ae8cbecdaf98400d3f2146d0292c8f726fd
NickTsioutsias <117465959+NickTsioutsias@users.noreply.github.com>
2022-11-28 08:42:44 +0200

    all triggers

---
+++ TRIGGERS.sql
@@ -0,0 +1,114 @@
+DELIMITER //
+CREATE TRIGGER tri_1 AFTER INSERT
+ON rental
+FOR EACH ROW
+BEGIN 
+	IF ROW_COUNT() > 0 THEN
+		INSERT INTO log VALUES
+		(NEW.customer_id,NOW(),'Success','INSERT','rental');
+	ELSE INSERT INTO log VALUES
+		(NULL,NOW(),'Fail','INSERT','rental');
+	ENDIF;	
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_2 AFTER UPDATE
+ON rental
+FOR EACH ROW
+BEGIN
+	IF ROW_COUNT() > 0 THEN 
+		INSERT INTO log VALUES 
+		(NEW.customer_id,NOW(),'Success','UPDATE','rental');
+	ELSE 
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Fail','UPDATE','rental');
+	ENDIF;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_3 AFTER DELETE
+ON rental
+FOR EACH ROW
+BEGIN
+	IF ROW_COUNT() > 0 THEN
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Success','DELETE','rental');
+	ELSE
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Fail','DELETE','rental');
+	ENDIF;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_4 AFTER INSERT
+ON payment
+FOR EACH ROW
+BEGIN
+	IF ROW_COUNT() > 0 THEN
+		INSERT INTO log VALUES
+		(NEW.customer_id,NOW(),'Success','INSERT','payment');
+	ELSE INSERT INTO log VALUES
+		(NULL,NOW(),'Fail','INSERT','payment');
+	ENDIF;	
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_5 AFTER UPDATE
+ON payment
+FOR EACH ROW
+BEGIN
+	IF ROW_COUNT() > 0 THEN 
+		INSERT INTO log VALUES 
+		(NEW.customer_id,NOW(),'Success','UPDATE','payment');
+	ELSE 
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Fail','UPDATE','payment');
+	ENDIF;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_6 AFTER DELETE
+ON payment
+FOR EACH ROW
+BEGIN
+	IF ROW_COUNT() > 0 THEN
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Success','DELETE','payment');
+	ELSE
+		INSERT INTO log VALUES
+		(OLD.customer_id,NOW(),'Fail','DELETE','payment');
+	ENDIF;
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_7 BEFORE INSERT 
+ON payment 
+FOR EACH ROW
+BEGIN
+		
+END//
+DELIMITER ;
+
+
+DELIMITER //
+CREATE TRIGGER tri_8 BEFORE UPDATE
+ON USER
+FOR EACH ROW
+BEGIN
+	IF email LIKE'%customer.org' THEN
+	SIGNAL SQLSTATE '45000' SET MESSAGE TEXT = 'You cannot change your personal data.';
+	ENDIF;
+END//
+DELIMITER ;
